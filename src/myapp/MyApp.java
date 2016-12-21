ed33bf4faf5447ecf013f4c284758f7b8ed3e4c3
maquan <maquan@maquandeMacBook-Pro.local>
2016-12-22 01:02:40 +0800

    project for testing

---
+++ src/myapp/MyApp.java
@@ -0,0 +1,16 @@
+/**
+ * MyApp.java
+ * myapp
+ */
+package myapp;
+
+public class MyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Enjoy gitHub!<0>_<0>");
+	}
+
+}
